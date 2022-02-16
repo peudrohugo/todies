4ac7005b03146fc682354544131f83fbcfa3f512
Pedro Hugo <pedrohugo.ov09@outlook.com>
2022-02-16 12:00:56 -0300

    refactor: completedTodies attribute and function added to ListProps interface

--- src/interfaces/ListProps.ts
+++ src/interfaces/ListProps.ts
@@ -3,4 +3,6 @@ import { Tody } from "./Tody";
 export interface ListProps {
     todies: Tody[];
     setTodies: React.Dispatch<React.SetStateAction<Tody[]>>;
+    completedTodies: Tody[];
+    setCompletedTodies: React.Dispatch<React.SetStateAction<Tody[]>>;
 }
